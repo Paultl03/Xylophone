9e1f6d31e25e393eaafa7a2cb38335491758b0df
Paultl03 <Plogan39@gmail.com>
2019-08-27 09:11:39 -0700

    Update  VIewController.swift
    Debugged : Index Out of Range

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 import AVFoundation
 
-class ViewController: UIViewController{
+class ViewController: UIViewController, AVAudioPlayerDelegate{
+    
+    var audioPlayer : AVAudioPlayer!
+    let soundArray = ["note1" , "note2" , "note3" , "note4" , "note5" , "note6" , "note7"]
+    
     
 
     override func viewDidLoad() {
@@ -18,10 +22,37 @@ class ViewController: UIViewController{
 
 
 
+    
     @IBAction func notePressed(_ sender: UIButton) {
-         
         
-  
-    }
-}
+        var selectedSoundFileName : String = soundArray[sender.tag - 1]
+        
+        
+        print(selectedSoundFileName)
+      playSound()
+        
+       
 
+}
+    
+    func playSound() {
+        
+        let soundURL = Bundle.main.url(forResource: "note1", withExtension: "wav")
+        
+        do {
+            
+            audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
+            
+        }
+            
+        catch {
+            print(error)
+            
+        }
+        
+        audioPlayer.play()
+        
+    }
+    
+
+}
